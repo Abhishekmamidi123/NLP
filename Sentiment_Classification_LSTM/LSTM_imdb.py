5d9bdbe3b31af2e46a05eb48769240f34df8483a
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-01 15:44:53 +0530

    Small changes

--- Sentiment_Classification_LSTM/LSTM_imdb.py
+++ Sentiment_Classification_LSTM/LSTM_imdb.py
@@ -22,7 +22,7 @@ model.add(Dense(1, activation = 'sigmoid'))
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 print model.summary()
 
-model.fit(X_train, y_train, epochs=3, batch_size=32, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=5, batch_size=32, validation_data=(X_test, y_test))
 score_train, accuracy_train = model.evaluate(X_train, y_train)
 print score_train, accuracy_train
 score_test, accuracy_test = model.evaluate(X_test, y_test)
