fc4a6fbd12db9888c8d4b69f18570b9ed3bc3abc
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-01 16:28:38 +0530

    Dropout + Visualize

--- Sentiment_Classification_LSTM/LSTM_imdb.py
+++ Sentiment_Classification_LSTM/LSTM_imdb.py
@@ -1,10 +1,10 @@
 import numpy as np
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, LSTM, Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
+from keras.utils import plot_model
 
 np.random.seed(7)
 number_of_words = 5000
@@ -17,10 +17,13 @@ X_test = sequence.pad_sequences(X_test, max_length_of_input)
 embedding_vector_length = 32
 model = Sequential()
 model.add(Embedding(number_of_words, embedding_vector_length, input_length = max_length_of_input))
+# Uncomment the below line if you want to use Dropout
+# model.add(Dropout(0.2))
 model.add(LSTM(100))
 model.add(Dense(1, activation = 'sigmoid'))
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 print model.summary()
+plot_model(model, to_file='model_LSTM.png', show_shapes=True, show_layer_names=True)
 
 model.fit(X_train, y_train, epochs=5, batch_size=32, validation_data=(X_test, y_test))
 score_train, accuracy_train = model.evaluate(X_train, y_train)
