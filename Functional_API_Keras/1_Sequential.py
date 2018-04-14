57796ede27902d03943fa21a2b19c6648d229fcf
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-14 23:28:56 +0530

    Sequential model - Small changes

--- Functional_API_Keras/1_Sequential.py
+++ Functional_API_Keras/1_Sequential.py
@@ -1,8 +1,14 @@
-# Sequential model
+# Sequential model - Example code
 import numpy as np
 from keras.layers import Dense, LSTM
 from keras.models import Sequential
 
+# Define X
+# Define y
 model = Sequential()
 model.add(Dense(2, input_dim = 1))
-model.add(Dense(1))
+model.add(Dense(1, activation='softmax'))
+
+model.compile(loss = 'categorial_crossentropy', metrics = ['accuracy'])
+model.run(X, y)
+y_test = model.predict(X_test)
