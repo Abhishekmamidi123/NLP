243df81845dc3ad479f71172879b9f04bbd3d65d
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-14 23:37:53 +0530

    Using Functional API - Multilayer Perceptron

---
+++ Functional_API_Keras/2_Functional_Multilayer_Perceptron.py
@@ -0,0 +1,14 @@
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.utils import plot_model
+
+visible = Input(shape=(10,))
+hidden1 = Dense(10, activation = 'relu')(visible)
+hidden2 = Dense(20, activation = 'relu')(hidden1)
+hidden3 = Dense(10, activation = 'relu')(hidden2)
+output = Dense(1, activation = 'sigmoid')(hidden3)
+
+model = Model(inputs = visible, outputs = output)
+print model.summary()
+plot_model(model, to_file = 'multilayer_perceptron_graph.png')
