be4ca0641551f43c5166104ca0f832877869dc5c
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-15 00:51:39 +0530

    Functional API Keras- Shared_Feature_Extraction_Layer

--- Functional_API_Keras/1_Sequential.py
+++ Functional_API_Keras/1_Sequential.py
@@ -2,13 +2,12 @@
 import numpy as np
 from keras.layers import Dense, LSTM
 from keras.models import Sequential
+from keras.utils import plot_model
 
 # Define X
 # Define y
 model = Sequential()
 model.add(Dense(2, input_dim = 1))
 model.add(Dense(1, activation='softmax'))
-
-model.compile(loss = 'categorial_crossentropy', metrics = ['accuracy'])
-model.run(X, y)
-y_test = model.predict(X_test)
+model.summary()
+plot_model(model, '1_Sequential.png')
---
+++ Functional_API_Keras/6_Shared_Feature_Extraction_Layer.py
@@ -0,0 +1,23 @@
+# Shared feature Extraction Layer model
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten
+from keras.utils import plot_model
+from keras.layers.recurrent import LSTM
+from keras.layers.merge import concatenate
+
+visible = Input((100,1))
+extract1 = LSTM(10)(visible)
+
+fe_layer_11 = Dense(10, activation = 'relu')(extract1)
+
+fe_layer_21 = Dense(10, activation = 'relu')(extract1)
+fe_layer_22 = Dense(20, activation = 'relu')(fe_layer_21)
+fe_layer_23 = Dense(10, activation = 'relu')(fe_layer_22)
+
+merge = concatenate([fe_layer_11, fe_layer_23])
+
+output = Dense(1, activation = 'sigmoid')(merge)
+model = Model(inputs = visible, outputs = output)
+
+print model.summary()
+plot_model(model, '6_Shared_Extraction_Layer.png')
