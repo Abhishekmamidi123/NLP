7f68e46d21dd077817c8111802f15c5d7751b22f
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-15 01:31:42 +0530

    Functional API Keras- Multiple Output model

---
+++ Functional_API_Keras/8_Multiple_Output_Model.py
@@ -0,0 +1,20 @@
+# Multiple Output Model
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten
+from keras.utils import plot_model
+from keras.layers.recurrent import LSTM
+from keras.layers.merge import concatenate
+from keras.layers.wrappers import TimeDistributed
+
+visible = Input(shape = (100, 1))
+
+hidden = LSTM(10, return_sequences = True)(visible)
+
+class11 = LSTM(10)(hidden)
+class12 = Dense(10, activation = 'relu')(class11)
+output1 = Dense(1, activation = 'sigmoid')(class12)
+output2 = TimeDistributed(Dense(1, activation = 'linear'))(hidden)
+
+model = Model(inputs = visible, outputs = [output1, output2])
+print model.summary()
+plot_model(model, '8_Multiple_Output_Model.png')
